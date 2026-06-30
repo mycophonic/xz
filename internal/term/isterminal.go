88cbd556cd5861f8734acf2b3eb2a7be13fabbca
gemelen <oss@gemelen.net>
2026-06-30 12:32:58 -0300

    Extend gxz build to illumos
    An attempt to add illumos platform

--- internal/term/isterminal.go
+++ internal/term/isterminal.go
@@ -2,21 +2,16 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-//go:build darwin || dragonfly || freebsd || (linux && !appengine) || netbsd || openbsd
-// +build darwin dragonfly freebsd linux,!appengine netbsd openbsd
+//go:build darwin || dragonfly || freebsd || (linux && !appengine) || netbsd || openbsd || illumos
+// +build darwin dragonfly freebsd linux,!appengine netbsd openbsd illumos
 
 // Package term provides the IsTerminal function.
 package term
 
-import (
-	"syscall"
-	"unsafe"
-)
+import "golang.org/x/sys/unix"
 
 // IsTerminal returns true if the given file descriptor is a terminal.
 func IsTerminal(fd uintptr) bool {
-	var termios syscall.Termios
-	_, _, err := syscall.Syscall6(syscall.SYS_IOCTL, fd,
-		ioctlGetTermios, uintptr(unsafe.Pointer(&termios)), 0, 0, 0)
-	return err == 0
+	_, err := unix.IoctlGetTermios(int(fd), ioctlGetTermios)
+	return err == nil
 }
